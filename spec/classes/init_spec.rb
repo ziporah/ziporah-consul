bafb6cd94e9d21b850e39086982c83183b3b909c
ziporah <jo.vanvoorden@telenet.be>
2017-03-06 19:00:15 +0000

    initialize the full module

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 describe 'consul' do
 
+  let(:facts) { {:osfamily => 'RedHat', :operatingsystemrelease => 'RedHat Linux release 7.0'} }
+
   context 'with defaults for all parameters' do
     it { should contain_class('consul') }
   end
